811a25763f839a16dec738e6773eef4eb572883a
Ahad <ahad.karimi8@gmail.com>
2022-07-22 21:56:54 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import Search from "./Search/Search"
+function App() {
+  return (
+  <>
+  <Search />
+  </>
+  );
+}
+export default App;
---
+++ src/data.js
@@ -0,0 +1,72 @@
+import image1 from './images/01.jpg';
+import image2 from './images/02.jpg';
+import image3 from './images/03.jpg';
+import image4 from './images/04.jpg';
+import image5 from './images/05.jpg';
+import image6 from './images/06.jpg';
+import image7 from './images/07.jpg';
+import image8 from './images/08.jpg';
+
+const data = {
+cardData:[
+    {
+        id:1,
+        img:image1,
+        title:'Lignes de fuite',
+        desc:'Three high school friends reunite with each other as women in their 30s. As the night progresses, old tensions resurface and new facts are revealed, putting their friendship to the test.'
+    }
+    ,
+    {
+        id:2,
+        img:image2,
+        title:'Arsenault & fils',
+        desc:'Family Game is a Canadian crime drama film, directed by Rafaël Ouellet and released in 2022. Set in the Bas-Saint-Laurent region of Quebec, the film centres on the Arsenaults, a family who have been involved in illegal poaching and reselling of wild meat out of the back of the family-owned garage. '
+    }
+    ,
+    {
+        id:3,
+        img:image3,
+        title:'The Crusade',
+        desc:'The Crusade is a 2021 French drama film directed by Louis Garrel, from a screenplay by Garrel and Jean-Claude Carrière. It stars Garrel, Laetitia Casta and Joseph Engel. It had its world premiere at the Cannes Film Festival on 12 July 2021.'
+    }
+    ,
+    {
+        id:4,
+        img:image4,
+        title:'Babysitter',
+        desc:'Babysitter is a Canadian comedy-drama film, directed by Monia Chokri and slated to premiere in 2022.'
+    }
+    ,
+    {
+        id:5,
+        img:image5,
+        title:'The Passengers of the Night',
+        desc:'After being left by her husband, a single mother is responsible for raising her two teenage children. Taking a second job at a late-night radio show, she meets a troubled teen whom she invites into her home.'
+    }
+
+    ,
+    {
+        id:6,
+        img:image6,
+        title:'The Bad Guys',
+        desc:"After a lifetime of legendary heists, notorious criminals Mr. Wolf, Mr. Snake, Mr. Piranha, Mr. Shark and Ms. Tarantula are finally caught. To avoid a prison sentence, the animal outlaws must pull off their most challenging con yet -- becoming model citizens. Under the tutelage of their mentor, Professor Marmalade, the dubious gang sets out to fool the world that they're turning good."
+    }
+    ,
+    {
+        id:7,
+        img:image7,
+        title:'Minions: The Rise of Gru',
+        desc:'In the 1970s, young Gru tries to join a group of supervillains called the Vicious 6 after they oust their leader -- the legendary fighter Wild Knuckles. When the interview turns disastrous, Gru and his Minions go on the run with the Vicious 6 hot on their tails. Luckily, he finds an unlikely source for guidance -- Wild Knuckles himself -- and soon discovers that even bad guys need a little help from their friends.'
+    }
+
+    ,
+    {
+        id:8,
+        img:image8,
+        title:'The Black Phone',
+        desc:"Finney Shaw is a shy but clever 13-year-old boy who's being held in a soundproof basement by a sadistic, masked killer. When a disconnected phone on the wall starts to ring, he soon discovers that he can hear the voices of the murderer's previous victims -- and they are dead set on making sure that what happened to them doesn't happen to Finney."
+    }
+
+]
+}
+export default data;
